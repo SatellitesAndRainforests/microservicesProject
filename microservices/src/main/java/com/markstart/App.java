a0886781686f9608f31c1c07f8a6bab7430fdd9d
witch <markstart00@gmail.com>
2022-06-02 12:46:05 +0100

    first commit

---
+++ microservices/src/main/java/com/markstart/App.java
@@ -0,0 +1,13 @@
+package com.markstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
