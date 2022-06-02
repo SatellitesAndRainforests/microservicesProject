971ac8a8dfb0f0c37443066ded6be429b6ea8347
witch <markstart00@gmail.com>
2022-06-02 16:36:29 +0100

    customer microserver complete -HTTP API, (controller,service,JPA,) postgresDatabase

--- microservices/customer/src/main/java/com/microservices/customer/CustomerApplication.java
+++ microservices/customer/src/main/java/com/microservices/customer/CustomerApplication.java
@@ -1,4 +1,14 @@
 package com.microservices.customer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//the first microservice
+@SpringBootApplication
 public class CustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
 }
